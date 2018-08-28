c47c73efb909f8a01033493bbc630be2ba01ef63
Willie Rendek <rendekwb@gmail.com>
2018-08-27 21:23:17 -0400

    add scroll animations

--- javascript/main.js
+++ javascript/main.js
@@ -1,15 +1,37 @@
-function scrollToSection(e) {
-  document.getElementById(e.target.section).scrollIntoView({
-    behavior: 'smooth'
+function scrollToSection(event) {
+  var standartransitionend = (!!window.URL)?'webkitTransitionEnd':'transitionend';
+  $('#stripes-container').removeClass('fade-in');
+  $('#stripes-container').addClass('fade-out');
+  $('#stripes-container').one(standartransitionend, function(e) {
+    document.getElementById(event.target.section).scrollIntoView({
+      behavior: 'smooth'
+    });
   });
 }
 
-let aboutBtn = document.getElementById('about-button')
+let aboutBtn = document.getElementById('about-button');
 aboutBtn.addEventListener('click', scrollToSection);
 aboutBtn.section = 'about-section';
-let workBtn = document.getElementById('work-button')
+let workBtn = document.getElementById('work-button');
 workBtn.addEventListener('click', scrollToSection);
 workBtn.section = 'work-section';
 let contactBtn = document.getElementById('contact-button');
 contactBtn.addEventListener('click', scrollToSection);
 contactBtn.section = 'contact-section';
+
+window.addEventListener('scroll', function() {
+    var landingSection = document.getElementById('landing-section');
+    var rect = landingSection.getBoundingClientRect();
+    var elemTop = rect.top;
+    var elemBottom = rect.bottom;
+
+    // Partially visible elements return true:
+    var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+    if(isVisible) {
+      $('#stripes-container').addClass('fade-in');
+      $('#stripes-container').removeClass('fade-out');
+    } else if(((elemTop + elemBottom) / window.screenY) < 1) {
+      $('#stripes-container').addClass('fade-out');
+      $('#stripes-container').removeClass('fade-in');
+    }
+}, true);
